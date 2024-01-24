b0550068e5bae184fa96360fa5408f4c80fde10f
NhuNhui <giaqui1403@gmail.com>
2024-01-24 16:49:30 +0700

    first

--- src/main/bkool/parser/BKOOL.g4
+++ src/main/bkool/parser/BKOOL.g4
@@ -12,15 +12,11 @@ options {
 // The single quote itself can appear as two single quotes back to back in a string: 'isn''t'.
 
 
-program: EOF;
+program: (vardecl | funcdecl)+;
 
+vardecl: 'vardecl' ;
+funcdecl: 'funcdecl' ;
 
-FLOAT:NORMAL|SCIENCE1|SCIENCE2;
-fragment NORMAL:INT '.' [0-9]*;
-fragment SCIENCE1:INT '.' [0-9]+ EXP '-'? INT;
-fragment SCIENCE2:INT EXP '-'? INT;
-fragment INT:'0'|[1-9] [0-9]*;
-fragment EXP:'e'|'E';
 
 WS : [ \t\r\n]+ -> skip ; // skip spaces, tabs, newlines
 
--- target/BKOOLLexer.py
+++ target/BKOOLLexer.py
@@ -14,35 +14,22 @@ from lexererr import *
 
 def serializedATN():
     with StringIO() as buf:
-        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\7")
-        buf.write("R\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
-        buf.write("\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\3\2\3\2\3\2\5\2\33")
-        buf.write("\n\2\3\3\3\3\3\3\7\3 \n\3\f\3\16\3#\13\3\3\4\3\4\3\4\6")
-        buf.write("\4(\n\4\r\4\16\4)\3\4\3\4\5\4.\n\4\3\4\3\4\3\5\3\5\3\5")
-        buf.write("\5\5\65\n\5\3\5\3\5\3\6\3\6\3\6\7\6<\n\6\f\6\16\6?\13")
-        buf.write("\6\5\6A\n\6\3\7\3\7\3\b\6\bF\n\b\r\b\16\bG\3\b\3\b\3\t")
-        buf.write("\3\t\3\t\3\n\3\n\3\13\3\13\2\2\f\3\3\5\2\7\2\t\2\13\2")
-        buf.write("\r\2\17\4\21\5\23\6\25\7\3\2\6\3\2\62;\3\2\63;\4\2GGg")
-        buf.write("g\5\2\13\f\17\17\"\"\2U\2\3\3\2\2\2\2\17\3\2\2\2\2\21")
-        buf.write("\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2\2\3\32\3\2\2\2\5\34\3")
-        buf.write("\2\2\2\7$\3\2\2\2\t\61\3\2\2\2\13@\3\2\2\2\rB\3\2\2\2")
-        buf.write("\17E\3\2\2\2\21K\3\2\2\2\23N\3\2\2\2\25P\3\2\2\2\27\33")
-        buf.write("\5\5\3\2\30\33\5\7\4\2\31\33\5\t\5\2\32\27\3\2\2\2\32")
-        buf.write("\30\3\2\2\2\32\31\3\2\2\2\33\4\3\2\2\2\34\35\5\13\6\2")
-        buf.write("\35!\7\60\2\2\36 \t\2\2\2\37\36\3\2\2\2 #\3\2\2\2!\37")
-        buf.write("\3\2\2\2!\"\3\2\2\2\"\6\3\2\2\2#!\3\2\2\2$%\5\13\6\2%")
-        buf.write("\'\7\60\2\2&(\t\2\2\2\'&\3\2\2\2()\3\2\2\2)\'\3\2\2\2")
-        buf.write(")*\3\2\2\2*+\3\2\2\2+-\5\r\7\2,.\7/\2\2-,\3\2\2\2-.\3")
-        buf.write("\2\2\2./\3\2\2\2/\60\5\13\6\2\60\b\3\2\2\2\61\62\5\13")
-        buf.write("\6\2\62\64\5\r\7\2\63\65\7/\2\2\64\63\3\2\2\2\64\65\3")
-        buf.write("\2\2\2\65\66\3\2\2\2\66\67\5\13\6\2\67\n\3\2\2\28A\7\62")
-        buf.write("\2\29=\t\3\2\2:<\t\2\2\2;:\3\2\2\2<?\3\2\2\2=;\3\2\2\2")
-        buf.write("=>\3\2\2\2>A\3\2\2\2?=\3\2\2\2@8\3\2\2\2@9\3\2\2\2A\f")
-        buf.write("\3\2\2\2BC\t\4\2\2C\16\3\2\2\2DF\t\5\2\2ED\3\2\2\2FG\3")
-        buf.write("\2\2\2GE\3\2\2\2GH\3\2\2\2HI\3\2\2\2IJ\b\b\2\2J\20\3\2")
-        buf.write("\2\2KL\13\2\2\2LM\b\t\3\2M\22\3\2\2\2NO\13\2\2\2O\24\3")
-        buf.write("\2\2\2PQ\13\2\2\2Q\26\3\2\2\2\13\2\32!)-\64=@G\4\b\2\2")
-        buf.write("\3\t\2")
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\b")
+        buf.write(".\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
+        buf.write("\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\3\3\3\3\3\3\3\3\3\3")
+        buf.write("\3\3\3\3\3\3\3\3\4\6\4\"\n\4\r\4\16\4#\3\4\3\4\3\5\3\5")
+        buf.write("\3\5\3\6\3\6\3\7\3\7\2\2\b\3\3\5\4\7\5\t\6\13\7\r\b\3")
+        buf.write("\2\3\5\2\13\f\17\17\"\"\2.\2\3\3\2\2\2\2\5\3\2\2\2\2\7")
+        buf.write("\3\2\2\2\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\3\17\3\2")
+        buf.write("\2\2\5\27\3\2\2\2\7!\3\2\2\2\t\'\3\2\2\2\13*\3\2\2\2\r")
+        buf.write(",\3\2\2\2\17\20\7x\2\2\20\21\7c\2\2\21\22\7t\2\2\22\23")
+        buf.write("\7f\2\2\23\24\7g\2\2\24\25\7e\2\2\25\26\7n\2\2\26\4\3")
+        buf.write("\2\2\2\27\30\7h\2\2\30\31\7w\2\2\31\32\7p\2\2\32\33\7")
+        buf.write("e\2\2\33\34\7f\2\2\34\35\7g\2\2\35\36\7e\2\2\36\37\7n")
+        buf.write("\2\2\37\6\3\2\2\2 \"\t\2\2\2! \3\2\2\2\"#\3\2\2\2#!\3")
+        buf.write("\2\2\2#$\3\2\2\2$%\3\2\2\2%&\b\4\2\2&\b\3\2\2\2\'(\13")
+        buf.write("\2\2\2()\b\5\3\2)\n\3\2\2\2*+\13\2\2\2+\f\3\2\2\2,-\13")
+        buf.write("\2\2\2-\16\3\2\2\2\4\2#\4\b\2\2\3\5\2")
         return buf.getvalue()
 
 
@@ -52,24 +39,25 @@ class BKOOLLexer(Lexer):
 
     decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
 
-    FLOAT = 1
-    WS = 2
-    ERROR_CHAR = 3
-    UNCLOSE_STRING = 4
-    ILLEGAL_ESCAPE = 5
+    T__0 = 1
+    T__1 = 2
+    WS = 3
+    ERROR_CHAR = 4
+    UNCLOSE_STRING = 5
+    ILLEGAL_ESCAPE = 6
 
     channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
 
     modeNames = [ "DEFAULT_MODE" ]
 
     literalNames = [ "<INVALID>",
- ]
+            "'vardecl'", "'funcdecl'" ]
 
     symbolicNames = [ "<INVALID>",
-            "FLOAT", "WS", "ERROR_CHAR", "UNCLOSE_STRING", "ILLEGAL_ESCAPE" ]
+            "WS", "ERROR_CHAR", "UNCLOSE_STRING", "ILLEGAL_ESCAPE" ]
 
-    ruleNames = [ "FLOAT", "NORMAL", "SCIENCE1", "SCIENCE2", "INT", "EXP", 
-                  "WS", "ERROR_CHAR", "UNCLOSE_STRING", "ILLEGAL_ESCAPE" ]
+    ruleNames = [ "T__0", "T__1", "WS", "ERROR_CHAR", "UNCLOSE_STRING", 
+                  "ILLEGAL_ESCAPE" ]
 
     grammarFileName = "BKOOL.g4"
 
@@ -84,7 +72,7 @@ class BKOOLLexer(Lexer):
     def action(self, localctx:RuleContext, ruleIndex:int, actionIndex:int):
         if self._actions is None:
             actions = dict()
-            actions[7] = self.ERROR_CHAR_action 
+            actions[3] = self.ERROR_CHAR_action 
             self._actions = actions
         action = self._actions.get(ruleIndex, None)
         if action is not None:
